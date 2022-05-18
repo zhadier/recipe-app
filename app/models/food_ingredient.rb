72de1843dcdf9d713e9c6802670178228e2282c2
Zeeshan Haider <zhadier39@gmail.com>
2022-05-19 04:03:00 +0500

    Add method to calculate total items

--- app/models/food_ingredient.rb
+++ app/models/food_ingredient.rb
@@ -2,6 +2,24 @@ class FoodIngredient < ApplicationRecord
   belongs_to :food
   belongs_to :receipe
 
+  after_create :add_total_items
+
+  before_destroy :delete_total_items
+
+  private
+
+  def add_total_items
+    receipe.increment!(:total_food_items)
+    current_price = receipe.total_price
+    receipe.update(total_price: (current_price + (self.Quantity * food.price)))
+  end
+
+  def delete_total_items
+    receipe.decrement!(:total_food_items)
+    current_price = receipe.total_price
+    receipe.update(total_price: (current_price - (self.Quantity * food.price)))
+  end
+
   def food_name
     food.name
   end
