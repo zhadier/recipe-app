40fd56b03145f81785d43fa663786444fc9ee65e
Aime Malaika <aimemalaika1995@gmail.com>
2022-05-18 21:31:08 +0200

    Add description to receipe

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_18_174434) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_18_192245) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_18_174434) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.string "Description"
     t.index ["user_id"], name: "index_receipes_on_user_id"
   end
 
